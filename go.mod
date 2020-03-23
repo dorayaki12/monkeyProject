9eeee89307c5bc2dd6128bace1f7ea72bb992c05
dorayaki <ytoriyama@gmail.com>
2020-03-23 16:16:47 +0900

    SDKを1.14に変更

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module monkeyProject
 
-go 1.13
+go 1.14
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `=+(){},;+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -20,6 +20,7 @@ func TestNextToken(t *testing.T) {
 		{token.RBRACE, "}"},
 		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
+		{token.PLUS, "+"},
 		{token.EOF, ""},
 	}
 
